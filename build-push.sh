dfad522473dba5d0a1c5b86209b8160efc36c3dc
Abby <abby@mocalabs.dev>
2026-05-07 21:34:09 -0500

    corrected a typo in build-push.sh, and triggering another build in woodpecker to test change in config to add plugins/docker to WOODPECKER_PLUGINS_PRIVILEGED

--- build-push.sh
+++ build-push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Building blog container ..."
-docker build --no-cache -t mocalabs/blog:latest .
+docker build --no-cache -t registry.mocalabs.dev/mocalabs/blog:latest .
 echo "Pushing container to registry ..."
-docker push mocalabs/blog:latest
+docker push registry.mocalabs.dev/mocalabs/blog:latest
 echo "Done building and pushing blog container to registry"
