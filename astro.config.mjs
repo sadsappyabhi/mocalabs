5e076c5aac43a20cff1b33399c1333b902d29c28
Abby <abby@mocalabs.dev>
2026-05-16 09:22:42 -0500

    Updated base url in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  site: "https://sadsappyabhi.github.io",
+  site: "https://www.mocalabs.dev",
   integrations: [mdx()],
   markdown: {
     shikiConfig: {
